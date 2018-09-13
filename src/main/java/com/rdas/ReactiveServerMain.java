534e0d86eb920e6ceacc641473bf3de144fb44e5
Rana Das <rana.pratap.das@gmail.com>
2018-09-13 05:31:03 +0100

    first commit with woking main

---
+++ src/main/java/com/rdas/ReactiveServerMain.java
@@ -0,0 +1,12 @@
+package com.rdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveServerMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveServerMain.class, args);
+	}
+}
